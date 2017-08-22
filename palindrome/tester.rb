a782ff8e7004c66c32aad9d12d64c7103df4e4d5
Nathan White <najowhit@iupui.edu>
2017-08-21 23:58:12 -0400

    Covered most test cases - temp solution for #2

--- palindrome/tester.rb
+++ palindrome/tester.rb
@@ -2,6 +2,10 @@ class Tester
   class T1
     def palindrome?(string)
       # first implementation
+      return false if !string.is_a?(String)
+      return false if string.length == 0
+
+
       if string == string.reverse
         return true
       else
@@ -14,6 +18,16 @@ class Tester
   class T2
     def palindrome?(string)
       # second implementation
+      return false if !string.is_a?(String)
+      return false if string.length == 0
+
+
+      if string == string.reverse
+        return true
+      else
+        return false
+      end
+
     end
   end
 end
