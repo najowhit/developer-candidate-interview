aeb9307324e14ae7c813e47c6bf7bb50f8e18538
Nathan White <najowhit@iupui.edu>
2017-08-23 18:38:41 -0400

    palindrome implementation one complete

--- palindrome/tester.rb
+++ palindrome/tester.rb
@@ -2,14 +2,15 @@ class Tester
   class T1
     def palindrome?(string)
       # first implementation
+
+      # Cover edge cases
+      return false if string.nil?
       return false if !string.is_a?(String)
       return false if string.length == 0
 
+      # Creates a substring from the string consisting of only lowercase alpha characters
       string = string.gsub(/[^\p{Alpha}]/, '').downcase
 
-      print "\n"
-      print string
-      print "\n"
 
       if string == string.reverse
         return true
@@ -23,20 +24,13 @@ class Tester
   class T2
     def palindrome?(string)
       # second implementation
+      # Cover edge cases
+      return false if string.nil?
       return false if !string.is_a?(String)
       return false if string.length == 0
 
+      # Creates a substring from the string consisting of only lowercase alpha characters
       string = string.gsub(/[^\p{Alpha}]/, '').downcase
-
-      print "\n"
-      print string
-      print "\n"
-
-      if string == string.reverse
-        return true
-      else
-        return false
-      end
 
     end
   end
